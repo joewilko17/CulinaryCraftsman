99572f06e17227cec1bb52f99433d2c05798add8
joewilko17 <joewilko17@gmail.com>
2025-05-21 18:42:32 +0100

    feat: initial backend added

---
+++ backend/src/main/java/cc/culinarycraftsman/CulinaryCraftsmanApplication.java
@@ -0,0 +1,13 @@
+package cc.culinarycraftsman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryCraftsmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinaryCraftsmanApplication.class, args);
+	}
+
+}
